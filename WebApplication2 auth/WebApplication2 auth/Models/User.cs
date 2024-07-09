950b7e70b864a616db219e9b181e62b57bd42172
Aryan Parmar <aryanparmarz24@gecg28.ac.in>
2024-07-09 15:41:38 +0530

    Add files via upload

---
+++ WebApplication2 auth/WebApplication2 auth/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication2_auth.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+    public class RequestUser
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
